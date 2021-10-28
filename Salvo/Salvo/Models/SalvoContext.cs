a44eb430f8aee58a79861b3506e71783e743f454
Ramiro <alegre.ramiro222@gmail.com>
2021-10-28 20:32:28 -0300

    primer commit

---
+++ Salvo/Salvo/Models/SalvoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Salvo.Models
+{
+    public class SalvoContext : DbContext
+    {
+        public SalvoContext(DbContextOptions<SalvoContext> options)
+            :base(options)
+        {
+        }
+    }
+}
