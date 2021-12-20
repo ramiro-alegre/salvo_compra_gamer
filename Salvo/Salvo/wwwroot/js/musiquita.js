cb1dd3bc316f10eaa239ddd99810e87ad8095a41
Ramiro <alegre.ramiro222@gmail.com>
2021-12-20 20:45:35 -0300

    Versión 1.1

--- Salvo/Salvo/wwwroot/js/musiquita.js
+++ Salvo/Salvo/wwwroot/js/musiquita.js
@@ -1,17 +1,19 @@
-const cargarSonido = function(fuente) {
-    const sonido = document.createElement("audio");
-    sonido.src = fuente;
-    sonido.loop = true;
-    sonido.setAttribute("preload", "auto");
-    sonido.setAttribute("controls", "none");
-    sonido.style.display = "none"; // <-- oculto
-    document.body.appendChild(sonido);
-    return sonido;
+const cargarSonido = function (fuente) {
+	const sonido = document.createElement("audio");
+	sonido.src = fuente;
+	sonido.loop = true;
+	sonido.setAttribute("preload", "auto");
+	sonido.setAttribute("controls", "none");
+	sonido.style.display = "none"; // <-- oculto
+	document.body.appendChild(sonido);
+	return sonido;
 };
 
 const $botonReproducir = document.querySelector("#btnReproducir"),
-    $botonPausar = document.querySelector("#btnPausar"),
-    $botonReiniciar = document.querySelector("#btnReiniciar");
+	$botonBajar = document.querySelector("#btnBajarVol"),
+	$botonSubir = document.querySelector("#btnSubirVol");
+
+let imagen = document.getElementById('dalePlay');
 
 // El sonido que podemos reproducir o pausar
 const sonido = cargarSonido("music/Battleship.mp3");
@@ -19,23 +21,34 @@ const sonido = cargarSonido("music/Battleship.mp3");
 let playing = 0;
 
 $botonReproducir.onclick = () => {
-    if (playing == 0) {
-        sonido.play();
-        sonido.muted = false;
-        playing = 1;
-    } else {
-        //sonido.pause();
-        sonido.muted = true;
-        playing = 0;
-    }
+	if (playing == 0) {
+		sonido.play();
+		sonido.muted = false;
+		playing = 1;
+	}
+	else {
+		//sonido.pause();
+		sonido.muted = true;
+		playing = 0;
+	}
 
-    FbotonOn();
+	FbotonOn();
 };
-$botonPausar.onclick = () => {
-    sonido.pause();
+$botonBajar.onclick = () => {
+	if (sonido.volume > 0)
+		sonido.volume -= 0.25;
+	if (sonido.volume == 0) {
+		sonido.muted = true;
+	}
+	FbotonOn();
 };
-$botonReiniciar.onclick = () => {
-    sonido.currentTime = 0;
+$botonSubir.onclick = () => {
+	if (sonido.volume < 1)
+		sonido.volume += 0.25;
+	if (sonido.volume != 0) {
+		sonido.muted = false;
+	}
+	FbotonOn();
 };
 
 // function FbotonOn() {
@@ -46,8 +59,9 @@ $botonReiniciar.onclick = () => {
 // }
 
 function FbotonOn() {
-    var imagen = document.getElementById('dalePlay');
-    if (playing == 0) {
-        imagen.src = "img/mute.png";
-    } else imagen.src = "img/play.png";
+	//var imagen = document.getElementById('dalePlay');
+	if (sonido.muted) {
+		imagen.src = "img/mute.png";
+	}
+	else imagen.src = "img/play.png";
 }
